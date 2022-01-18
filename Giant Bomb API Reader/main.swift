1726b8fcb63a25a40b629d633576de8ce64f888d
Duncan Oliver <duncan.oliver@gmail.com>
2022-01-18 15:21:03 -0800

    Get response dictionary

--- Giant Bomb API Reader/main.swift
+++ Giant Bomb API Reader/main.swift
@@ -6,6 +6,41 @@
 //
 
 import Foundation
+import WebKit
 
-print("Hello, World!")
+guard let apiURL = URL(string: "https://www.giantbomb.com/api/documentation/") else
+{
+  print("Failed to get URL")
+  exit(EXIT_FAILURE)
+}
+do
+{
+  let documentationXMLDocument = try XMLDocument(contentsOf: apiURL, options: [.documentTidyHTML])
+  
+  // Grab the "Response" format to create the encapsulation structure
+  if let responseNode = try? documentationXMLDocument.nodes(forXPath: "//*[@id='default-content']/div/table[1]").first
+  {
+    print(getPath(forTableNode: responseNode))
+  }
+  else
+  {
+    print("Unable to find response definition table.")
+  }
+  
+}
+catch
+{
+  print(error)
+}
 
+func getPath(forTableNode tableNode: XMLNode) -> Dictionary<String, Any>
+{
+  var responseDictionary = [String : Any]()
+  for nextRow in (try? tableNode.nodes(forXPath: "tbody/tr")) ?? []
+  {
+    guard let nextPropertyName = try? nextRow.nodes(forXPath: "td[1]").first?.stringValue else { exit(EXIT_FAILURE) }
+    guard let nextPropertyDescription = try? nextRow.nodes(forXPath: "td[2]").first?.stringValue else { exit(EXIT_FAILURE) }
+    responseDictionary[nextPropertyName] = ["description": nextPropertyDescription]
+  }
+  return responseDictionary
+}
